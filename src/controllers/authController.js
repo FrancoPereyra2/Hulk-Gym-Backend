59efae15b513cf5e60d18d00a834815e9bce4fde
Franco Pereyra <francopereyra325@gmail.com>
2026-05-12 16:28:07 -0300

    actualizar backend

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -94,11 +94,6 @@ const crearTransporteEmail = () => {
   });
 };
 
-console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
-console.log("FRONTEND_URL_ALT:", process.env.FRONTEND_URL_ALT);
-console.log("frontendUrl resultado:", frontendUrl);
-console.log("urlCambioPassword:", urlCambioPassword);
-
 const enviarEmailBienvenida = async (usuario, passwordTemporal, tokenCambio) => {
   const transporter = crearTransporteEmail();
   const frontendUrl = (
